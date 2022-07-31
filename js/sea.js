1615d519bb373346ad0578777f2cace616ea6f03
user <evgeniiperkin@bk.ru>
2022-07-31 12:18:10 +0300

    saving learn

--- js/sea.js
+++ js/sea.js
@@ -16,10 +16,50 @@ var view = {
 
 };
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
-view.displayMessage("Tpa tap, is this thing on?");
+var model = {
+	bordSize: 7, 	// размер сетки игрового поля
+	numShips: 3,	// количество кораблей в игре
+	shipLength: 3,	// длина каждого корабля (в клетках)
+	shipsSunk: 0,	// количество потопленных кораблей
+    ships: [{locations: ["06", "16", "26"], hits: ["", "", ""]},
+	{locations: ["24", "34", "44"], hits: ["", "hit", ""]},
+	{locations: ["10", "11", "12"], hits: ["", "", ""]}], 
+	fire: function(guess) {
+		for(var i = 0; i < this.numShips; i++) {
+			var ship = this.ships[i];
+			var locations = ship.locations;
+			var index = locations.indexOf(guess);
+			if (index >= 0){
+				ship.hits[index] = "hit";
+				view.displayHit(guess);
+				view.displayMessage("HIT!!");
+				if (this.isSunk(ship)){
+					view.displayMessage("You sank my battleship!");
+					this.shipsSunk++;
+				}
+				return true;
+			}
+		}
+		view.displayMiss(guess);
+		view.displayMessage("You missed.");
+		return false;
+	},
+	isSunk: function(ship) {
+		for (var i = 0; i < this.shipLength; i++){
+			if (ship.hits[i] !== "hit") {
+				return false;
+			}
+		}
+		return true;
+	}
+};
+model.fire("53");
+model.fire("06");
+model.fire("16");
+model.fire("26");
+model.fire("34");
+model.fire("24");
+model.fire("44");
+model.fire("12");
+model.fire("11");
+model.fire("10");
