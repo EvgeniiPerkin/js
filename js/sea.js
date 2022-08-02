53fb7ac212928dc35fca27eb80856367e73309ef
user <evgeniiperkin@bk.ru>
2022-08-02 10:19:09 +0300

    saving learn

--- js/sea.js
+++ js/sea.js
@@ -51,15 +51,108 @@ var model = {
 			}
 		}
 		return true;
+	},
+	generateShipLoacations: function () {
+		var locations;
+		for (var i = 0; i < this.numShips; i++) {
+			do {
+				locations = this.generateShip();
+			} while(this.collision(locations));
+			this.ships[i].locations = locations;
+		}
+	},
+	generateShip: function() {
+		var direction = Math.floor(Math.random() * 2);
+		var row, col;
+
+		if (direction === 1) {
+			row = Math.floor(Math.random() * this.bordSize);
+			col = Math.floor(Math.random() * (this.bordSize - this.shipLength));
+		} else {
+			row = Math.floor(Math.random() * (this.bordSize - this.shipLenght));
+			col = Math.floor(Math.random() * this.bordSize);
+		}
+		
+		var newShipLocations = [];	
+		for (var i = 0; i < this.shipLength; i++) {	
+			if (direction === 1) {
+				newShipLocations.push(row + "" + (col + i));
+			} else {
+				newShipLocations.push((row + i) + "" + col);
+			}	
+		}
+		return newShipLocations;
+	},
+	collision: function(location) {
+		for (var i = 0; i < this.numShips; i++) {
+			var ship = model.ship[i];
+			for (var j = 0; j < locations.length; j++) {
+				if (ship.location.indexOf(locations[j] >= 0)){
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 };
-model.fire("53");
-model.fire("06");
-model.fire("16");
-model.fire("26");
-model.fire("34");
-model.fire("24");
-model.fire("44");
-model.fire("12");
-model.fire("11");
-model.fire("10");
+
+function parseGuess(guess) {
+	var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+	if (guess === null || guess.length !== 2) {
+		alert("Oops, please enter a letter and a number on the board.")
+	} else {
+		var firstChar = guess.charAt(0);
+		var row = alphabet.indexOf(firstChar);
+		var column = guess.charAt(1);
+		
+		if (isNaN(row) || isNaN(column)) {
+			alert("Oops, that isn't on the board.");
+		} else if (row < 0 || row >= model.bordSize ||
+			column < 0 || column >= model.bordSize) {
+			alert("Oops, that's off the board!")
+		} else {
+			return row + column;
+		}
+	}
+
+	return null;
+}
+
+var controller = {
+	guisses: 0,
+	progressGuess: function(guess) {
+		var location = parseGuess(guess);
+		if (location) {
+			this.guisses++;
+			var hit = model.fire(location);
+			if (hit && model.shipsSunk === model.numShips) {
+				view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
+			}
+		}
+	}
+};
+
+function init() {
+	var fireButton = document.getElementById("fireButton");
+	fireButton.onclick = handleFireButton;
+	var guessInput = document.getElementById("guessInput");
+	guessInput.onkeypress = handlerKeyPress;
+}
+
+function handlerKeyPress(e) {
+	var fireButton = document.getElementById("fireButton");
+	if (e.keyCode === 13) {
+		fireButton.click();
+		return false;
+	}
+}
+
+function handleFireButton() {
+	var guessInput = document.getElementById("guessInput");
+	var guess = guessInput.value;
+	controller.progressGuess(guess);
+	guessInput.value = "";
+}
+
+window.onload = init;
